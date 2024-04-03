e7f3325e3ccc7bddd1ad0310bb230d0e14e4b402
Matt Kulka <92747257+MattKulka@users.noreply.github.com>
2024-04-03 19:08:04 -0400

    Finished Landing Page

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+    SuperQuality,
+    SpecialOffer
+}
